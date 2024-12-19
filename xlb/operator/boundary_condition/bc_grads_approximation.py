1fdaf22cc8f5ea9182ef8537685a70a2ab9f47da
Hesam Salehipour <hesam.salehipour@autodesk.com>
2024-12-19 17:22:07 -0500

    added hybrid bc with different methods

--- xlb/operator/boundary_condition/bc_grads_approximation.py
+++ xlb/operator/boundary_condition/bc_grads_approximation.py
@@ -28,25 +28,31 @@ from xlb.operator.boundary_condition.boundary_condition_registry import (
 )
 
 
-class GradsApproximationBC(BoundaryCondition):
+class HybridBC(BoundaryCondition):
     """
-    Purpose: Using Grad's approximation to represent fpop based on macroscopic inputs used for outflow [1] and
-    Dirichlet BCs [2]
-    [1] S. Chikatamarla, S. Ansumali, and I. Karlin, "Grad's approximation for missing data in lattice Boltzmann
-        simulations", Europhys. Lett. 74, 215 (2006).
-    [2] Dorschner, B., Chikatamarla, S. S., Bösch, F., & Karlin, I. V. (2015). Grad's approximation for moving and
-        stationary walls in entropic lattice Boltzmann simulations. Journal of Computational Physics, 295, 340-354.
+    The hybrid BC methods in this boundary condition have been originally developed by H. Salehipour and are inspired from
+    various previous publications, in particular [2]. The reformulations are aimed to provide local formulations that are
+    computationally efficient and numerically stable at exessively large Reynolds numbers.
 
+    [1] Dorschner, B., Chikatamarla, S. S., Bösch, F., & Karlin, I. V. (2015). Grad's approximation for moving and
+        stationary walls in entropic lattice Boltzmann simulations. Journal of Computational Physics, 295, 340-354.
     """
 
     def __init__(
         self,
+        bc_method,
         velocity_set: VelocitySet = None,
         precision_policy: PrecisionPolicy = None,
         compute_backend: ComputeBackend = None,
         indices=None,
         mesh_vertices=None,
     ):
+        assert bc_method in [
+            "bounceback_regularized",
+            "bounceback_grads",
+        ], f"type = {bc_method} not supported! Use 'bounceback_regularized' or 'bounceback_grads'."
+        self.bc_method = bc_method
+
         # TODO: the input velocity must be suitably stored elesewhere when mesh is moving.
         self.u = (0, 0, 0)
 
@@ -67,7 +73,7 @@ class GradsApproximationBC(BoundaryCondition):
         self.momentum_flux = MomentumFlux()
 
         # This BC needs implicit distance to the mesh
-        self.needs_mesh_distance = True
+        self.needs_mesh_distance = False
 
         # If this BC is defined using indices, it would need padding in order to find missing directions
         # when imposed on a geometry that is in the domain interior
@@ -98,93 +104,23 @@ class GradsApproximationBC(BoundaryCondition):
         return
 
     def _construct_warp(self):
+        # load helper functions
+        from xlb.helper.bc_warp_functions import regularize_fpop, grads_approximate_fpop, interpolated_bounceback
+
         # Set local variables and constants
         _c = self.velocity_set.c
         _q = self.velocity_set.q
         _d = self.velocity_set.d
         _w = self.velocity_set.w
-        _qi = self.velocity_set.qi
         _opp_indices = self.velocity_set.opp_indices
         _f_vec = wp.vec(self.velocity_set.q, dtype=self.compute_dtype)
         _u_vec = wp.vec(self.velocity_set.d, dtype=self.compute_dtype)
         _u_wall = _u_vec(self.u[0], self.u[1], self.u[2]) if _d == 3 else _u_vec(self.u[0], self.u[1])
         # diagonal = wp.vec3i(0, 3, 5) if _d == 3 else wp.vec2i(0, 2)
 
-        @wp.func
-        def regularize_fpop(
-            missing_mask: Any,
-            rho: Any,
-            u: Any,
-            fpop: Any,
-        ):
-            """
-            Regularizes the distribution functions by adding non-equilibrium contributions based on second moments of fpop.
-            """
-            # Compute momentum flux of off-equilibrium populations for regularization: Pi^1 = Pi^{neq}
-            feq = self.equilibrium.warp_functional(rho, u)
-            f_neq = fpop - feq
-            PiNeq = self.momentum_flux.warp_functional(f_neq)
-
-            # Compute double dot product Qi:Pi1 (where Pi1 = PiNeq)
-            nt = _d * (_d + 1) // 2
-            for l in range(_q):
-                QiPi1 = self.compute_dtype(0.0)
-                for t in range(nt):
-                    QiPi1 += _qi[l, t] * PiNeq[t]
-
-                # assign all populations based on eq 45 of Latt et al (2008)
-                # fneq ~ f^1
-                fpop1 = self.compute_dtype(4.5) * _w[l] * QiPi1
-                fpop[l] = feq[l] + fpop1
-            return fpop
-
-        @wp.func
-        def grads_approximate_fpop(
-            missing_mask: Any,
-            rho: Any,
-            u: Any,
-            f_post: Any,
-        ):
-            # Purpose: Using Grad's approximation to represent fpop based on macroscopic inputs used for outflow [1] and
-            # Dirichlet BCs [2]
-            # [1] S. Chikatax`marla, S. Ansumali, and I. Karlin, "Grad's approximation for missing data in lattice Boltzmann
-            #   simulations", Europhys. Lett. 74, 215 (2006).
-            # [2] Dorschner, B., Chikatamarla, S. S., Bösch, F., & Karlin, I. V. (2015). Grad's approximation for moving and
-            #    stationary walls in entropic lattice Boltzmann simulations. Journal of Computational Physics, 295, 340-354.
-
-            # Note: See also self.regularize_fpop function which is somewhat similar.
-
-            # Compute pressure tensor Pi using all f_post-streaming values
-            Pi = self.momentum_flux.warp_functional(f_post)
-
-            # Compute double dot product Qi:Pi1 (where Pi1 = PiNeq)
-            nt = _d * (_d + 1) // 2
-            for l in range(_q):
-                # if missing_mask[l] == wp.uint8(1):
-                QiPi = self.compute_dtype(0.0)
-                for t in range(nt):
-                    if t == 0 or t == 3 or t == 5:
-                        QiPi += _qi[l, t] * (Pi[t] - rho / self.compute_dtype(3.0))
-                    else:
-                        QiPi += _qi[l, t] * Pi[t]
-
-                # Compute c.u
-                cu = self.compute_dtype(0.0)
-                for d in range(self.velocity_set.d):
-                    if _c[d, l] == 1:
-                        cu += u[d]
-                    elif _c[d, l] == -1:
-                        cu -= u[d]
-                cu *= self.compute_dtype(3.0)
-
-                # change f_post using the Grad's approximation
-                f_post[l] = rho * _w[l] * (self.compute_dtype(1.0) + cu) + _w[l] * self.compute_dtype(4.5) * QiPi
-
-            return f_post
-
         # Construct the functionals for this BC
         @wp.func
-        def functional_method1(
+        def hybrid_bounceback_regularized(
             index: Any,
             timestep: Any,
             missing_mask: Any,
@@ -193,40 +129,54 @@ class GradsApproximationBC(BoundaryCondition):
             f_pre: Any,
             f_post: Any,
         ):
-            # NOTE: this BC has been reformulated to become entirely local and so has differences compared to the original paper.
-            #       Here we use the current time-step populations (f_pre = f_post_collision and f_post = f_post_streaming).
-            one = self.compute_dtype(1.0)
-            for l in range(_q):
-                # If the mask is missing then take the opposite index
-                if missing_mask[l] == wp.uint8(1):
-                    # The implicit distance to the boundary or "weights" have been stored in known directions of f_1
-                    # weight = f_1[_opp_indices[l], index[0], index[1], index[2]]
-                    weight = self.compute_dtype(0.5)
+            # Using regularization technique [1] to represent fpop using macroscopic values derived from interpolated bounceback scheme of [2].
+            # missing data in lattice Boltzmann.
+            # [1] Latt, J., Chopard, B., Malaspinas, O., Deville, M., Michler, A., 2008. Straight velocity
+            #     boundaries in the lattice Boltzmann method. Physical Review E 77, 056703.
+            # [2] Yu, D., Mei, R., Shyy, W., 2003. A uniﬁed boundary treatment in lattice boltzmann method,
+            #     in: 41st aerospace sciences meeting and exhibit, p. 953.
 
-                    # Use differentiable interpolated BB to find f_missing:
-                    f_post[l] = ((one - weight) * f_post[_opp_indices[l]] + weight * (f_pre[l] + f_pre[_opp_indices[l]])) / (one + weight)
+            # Apply interpolated bounceback first to find missing populations at the boundary
+            f_post = interpolated_bounceback(missing_mask, f_0, f_1, f_pre, f_post)
 
-                    # # Add contribution due to moving_wall to f_missing as is usual in regular Bouzidi BC
-                    # cu = self.compute_dtype(0.0)
-                    # for d in range(_d):
-                    #     if _c[d, l] == 1:
-                    #         cu += _u_wall[d]
-                    #     elif _c[d, l] == -1:
-                    #         cu -= _u_wall[d]
-                    # cu *= self.compute_dtype(-6.0) * _w[l]
-                    # f_post[l] += cu
+            # Compute density, velocity using all f_post-streaming values
+            rho, u = self.macroscopic.warp_functional(f_post)
+
+            # Regularize the resulting populations
+            feq = self.equilibrium.warp_functional(rho, u)
+            f_post = regularize_fpop(f_post, feq)
+            return f_post
+
+        @wp.func
+        def hybrid_bounceback_grads(
+            index: Any,
+            timestep: Any,
+            missing_mask: Any,
+            f_0: Any,
+            f_1: Any,
+            f_pre: Any,
+            f_post: Any,
+        ):
+            # Using Grad's approximation [1] to represent fpop using macroscopic values derived from interpolated bounceback scheme of [2].
+            # missing data in lattice Boltzmann.
+            # [1] Dorschner, B., Chikatamarla, S. S., Bösch, F., & Karlin, I. V. (2015). Grad's approximation for moving and
+            #    stationary walls in entropic lattice Boltzmann simulations. Journal of Computational Physics, 295, 340-354.
+            # [2] Yu, D., Mei, R., Shyy, W., 2003. A uniﬁed boundary treatment in lattice boltzmann method,
+            #     in: 41st aerospace sciences meeting and exhibit, p. 953.
+
+            # Apply interpolated bounceback first to find missing populations at the boundary
+            f_post = interpolated_bounceback(missing_mask, f_0, f_1, f_pre, f_post)
 
             # Compute density, velocity using all f_post-streaming values
             rho, u = self.macroscopic.warp_functional(f_post)
 
             # Compute Grad's appriximation using full equation as in Eq (10) of Dorschner et al.
-            f_post = regularize_fpop(missing_mask, rho, u, f_post)
-            # f_post = grads_approximate_fpop(missing_mask, rho, u, f_post)
+            f_post = grads_approximate_fpop(missing_mask, rho, u, f_post)
             return f_post
 
         # Construct the functionals for this BC
         @wp.func
-        def functional_method2(
+        def hybrid_bounceback_grads_method2(
             index: Any,
             timestep: Any,
             missing_mask: Any,
@@ -307,7 +257,10 @@ class GradsApproximationBC(BoundaryCondition):
             f_post = grads_approximate_fpop(missing_mask, rho_target, u_target, f_post)
             return f_post
 
-        functional = functional_method1
+        if self.bc_method == "bounceback_regularized":
+            functional = hybrid_bounceback_regularized
+        elif self.bc_method == "bounceback_grads":
+            functional = hybrid_bounceback_grads
 
         kernel = self._construct_kernel(functional)
 
