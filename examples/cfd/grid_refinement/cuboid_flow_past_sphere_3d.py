0e61c587c716ce02fe09a03ec6a05f1a0be74409
Hesam Salehipour <hesam.salehipour@autodesk.com>
2025-09-06 11:55:19 -0400

    Fixed the missing mask in MRES that was incorrect on +x, +y and +z bounds of the domain

--- examples/cfd/grid_refinement/cuboid_flow_past_sphere_3d.py
+++ examples/cfd/grid_refinement/cuboid_flow_past_sphere_3d.py
@@ -211,7 +211,7 @@ bc_left = RegularizedBC("velocity", profile=bc_profile(), indices=inlet)
 # bc_left = HybridBC(bc_method="bounceback_regularized", profile=bc_profile(), indices=inlet)
 # Alternatively, use a prescribed velocity profile
 # bc_left = RegularizedBC("velocity", prescribed_value=(u_max, 0.0, 0.0), indices=inlet)
-bc_walls = FullwayBounceBackBC(indices=walls)  # TODO: issues with halfway bounce back only here!
+bc_walls = HalfwayBounceBackBC(indices=walls)
 # bc_ground = FullwayBounceBackBC(indices=grid.boundary_indices_across_levels(level_data, box_side="front"))
 # bc_outlet = ExtrapolationOutflowBC(indices=outlet)
 bc_outlet = DoNothingBC(indices=outlet)
