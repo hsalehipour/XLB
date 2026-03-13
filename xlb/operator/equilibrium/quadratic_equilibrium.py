1bdc73285891f0ea6d28274c25536c5deb9daf33
Massimiliano Meneghin <massimiliano.meneghin@autodesk.com>
2026-03-13 21:36:41 +0100

    Fixing merge.

--- xlb/operator/equilibrium/quadratic_equilibrium.py
+++ xlb/operator/equilibrium/quadratic_equilibrium.py
@@ -60,11 +60,7 @@ class QuadraticEquilibrium(Equilibrium):
                 usqr = self.compute_dtype(1.5) * wp.dot(u, u)
 
                 # Compute feq
-                feq[l] = (
-                    self.compute_dtype(rho)
-                    * self.compute_dtype(_w[l])
-                    * (self.compute_dtype(1.0) + cu * (self.compute_dtype(1.0) + self.compute_dtype(0.5) * cu) - usqr)
-                )
+                feq[l] = rho * _w[l] * (self.compute_dtype(1.0) + cu * (self.compute_dtype(1.0) + self.compute_dtype(0.5) * cu) - usqr)
 
             return feq
 
